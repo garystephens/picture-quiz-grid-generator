db5c3919c80d317e6d43682a8092dc10ed3ede51
Gary Stephens <gary.p.stephens@gmail.com>
2020-11-19 18:03:40 +0000

    Move watermark to own component

--- src/components/pictureQuizGrid.js
+++ src/components/pictureQuizGrid.js
@@ -1,39 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import Draggable from 'react-draggable';
 import PictureQuizGridElement from './pictureQuizGridElement.js';
-import { addClassToElementTemporarily } from '../utils.js';
-import { usePrevious } from '../reactUtils.js';
+import DraggableWatermark from './draggableWatermark.js';
 
 function PictureQuizGrid(props) {
-    const [watermarkText, setWatermarkText] = useState(props.watermarkText);
-    const prevWatermarketText = usePrevious(watermarkText);
-
-    // On change of watermark text
-    useEffect(() => {
-        setWatermarkText(props.watermarkText);
-        // Highlight changes to the watermark with a temporary style change
-        if (
-            prevWatermarketText !== undefined &&
-            props.watermarkText !== prevWatermarketText
-        ) {
-            addClassToElementTemporarily('#watermarkText', 'highlight', 500);
-        }
-    }, [props.watermarkText]);
-
     return (
         <div id="pictureQuizGrid">
-            <Draggable>
-                <div id="watermark">
-                    <div
-                        id="watermarkText"
-                        className={props.watermarkVertical ? 'rotate90' : ''}
-                    >
-                        {' '}
-                        {watermarkText}
-                    </div>
-                </div>
-            </Draggable>
+            <DraggableWatermark
+                watermarkText={props.watermarkText}
+                watermarkVertical={props.watermarkVertical}
+            />
             <div
                 id="grid"
                 style={{ width: props.gridSize + '%' }}
