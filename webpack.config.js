fd586423baf989af7fc78db863f74d798f4e86eb
Gary Stephens <gary.p.stephens@gmail.com>
2022-12-01 22:31:01 +0000

    Upgrade webpack to v5, also upgrade associated loaders

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/js/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[hash].js',
+        filename: '[name].[contenthash].js',
     },
     devtool: 'source-map',
     devServer: {
